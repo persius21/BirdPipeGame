97cf4395b620152fd0604d1faccf2338c32ec51d
persius21 <72589227+persius21@users.noreply.github.com>
2023-09-11 21:36:53 +0800

    Base Version (Multiplayer WIP)

---
+++ Assets/Scripts/Netcode RPC for VS.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using Unity.VisualScripting;
+using System;
+
+public class NetcodeRPCforVS : NetworkBehaviour
+{
+
+    [ServerRpc]
+    public void VSServerRpc(string value){
+        CustomEvent.Trigger(gameObject, value);
+
+    }
+
+     [ServerRpc]
+    public void VSServerRpc(string value, int num){
+        CustomEvent.Trigger(gameObject, value, num);
+
+    }
+
+     [ServerRpc]
+    public void VSServerRpc(string value, Vector3 vector){
+        CustomEvent.Trigger(gameObject, value, vector);
+
+    }
+
+     [ServerRpc]
+    public void VSServerRpc(string value, float num){
+        CustomEvent.Trigger(gameObject, value, num);
+
+    }
+
+    [ClientRpc]
+    public void VSClientRPC(string value){
+         CustomEvent.Trigger(gameObject, value);
+    }
+
+    [ClientRpc]
+    public void VSClientRPC(string value, int num){
+         CustomEvent.Trigger(gameObject, value, num);
+    }
+
+    [ClientRpc]
+    public void VSClientRPC(string value, Vector3 vector){
+         CustomEvent.Trigger(gameObject, value, vector);
+    }
+
+    [ClientRpc]
+    public void VSClientRPC(string value, float num){
+         CustomEvent.Trigger(gameObject, value, num);
+    }
+
+}
