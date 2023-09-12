bb711f8deec18c028c8835eeb0485a64e0cb4065
persius21 <72589227+persius21@users.noreply.github.com>
2023-09-12 12:37:50 +0800

    September 12 2023
    -Added Buttons

---
+++ Assets/Scripts/PlayerSpawnRandomPosition.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using Unity.VisualScripting;
+
+public class PlayerSpawnRandom : NetworkBehaviour
+{
+    public override void OnNetworkSpawn(){
+        transform.position = new Vector3(Random.Range(-2,0),2,0);
+    }
+}
