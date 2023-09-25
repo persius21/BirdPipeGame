10f6b589edca1e0571302a4305c9625a73e48a61
persius21 <72589227+persius21@users.noreply.github.com>
2023-09-25 18:05:28 +0800

    Update PlayerCollision.cs

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -8,7 +8,7 @@ public class PlayerCollision : NetworkBehaviour
     // This method is called when a collision occurs with another GameObject's collider.
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Pipe") && IsOwner)
+        if (collision.gameObject.CompareTag("Pipe"))
         {
             // When a player collides with a pipe, change the isAlive boolean to false.
             isAlive = false;
