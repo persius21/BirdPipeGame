2fd1e45261e81af4998b248fd86b39f8d9deee5b
persius21 <72589227+persius21@users.noreply.github.com>
2023-09-25 13:33:39 +0800

    1:33PM

--- Assets/Scripts/PlayerScore.cs
+++ Assets/Scripts/PlayerScore.cs
@@ -5,17 +5,31 @@ public class PlayerScore : NetworkBehaviour
 {
     private int score = 0; // Store the player's score.
     private bool isColliding = false;
+    private bool isAlive = true;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("PointCollider")&& IsOwner)
         {
+            if(isAlive==true){
             // When a player enters a point collider, increase the player's score.
             score++;
-            isColliding = true;    
+            isColliding = true;
+            }    
             
         }
         else{
             isColliding = false;
+        }
+    }
+
+ private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Pipe"))
+        {
+            // When a player collides with a pipe, change the isAlive boolean to false.
+            isAlive = false;
+            
+            // You can add any additional logic here, such as playing a death animation or sound.
         }
     }
 
