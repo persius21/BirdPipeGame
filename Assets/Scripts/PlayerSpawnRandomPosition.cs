af42b3b024e8bea3bf7db313d0a4e9ad5a81b2a1
persius21 <145455839+vi2uba@users.noreply.github.com>
2023-10-03 21:03:47 +0800

    9:03PM

--- Assets/Scripts/PlayerSpawnRandomPosition.cs
+++ Assets/Scripts/PlayerSpawnRandomPosition.cs
@@ -8,6 +8,10 @@ public class PlayerSpawnRandom : NetworkBehaviour
 {
     public override void OnNetworkSpawn(){
         transform.position = new Vector3(Random.Range(-2,0),2,0);
+
+        transform.rotation = Quaternion.Euler(0, 90, 0);
+
+        
     }
 
 }
